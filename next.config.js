dc37715c1dcb7688361ccf0ae83ee59cf1f849c2
mythosmystery <axiysfire@gmail.com>
2021-10-27 00:57:51 -0500

    updated next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
    images: {
       loader: 'imgix',
-      path: 'https://mythosmystery.github.io',
+      path: 'https://mythosmystery.github.io/tailwind-portfolio',
    },
    assetPrefix: isProd ? '/tailwind-portfolio/' : '',
 };
