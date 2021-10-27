099600e194355c89a35648079e8785b6856c09a8
mythosmystery <axiysfire@gmail.com>
2021-10-27 00:51:14 -0500

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
    images: {
       loader: 'imgix',
       path: 'https://mythosmystery.github.io//',
    },
-};
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
    assetPrefix: isProd ? '/tailwind-portfolio/' : '',
 };
