52c51b52d91d2bdcb8e50629d77e68c6b739418e
mythosmystery <axiysfire@gmail.com>
2021-11-13 20:09:23 -0600

    minor ui changes

--- components/IconContainer.tsx
+++ components/IconContainer.tsx
@@ -8,10 +8,10 @@ interface IconContainer {
 export const IconContainer: React.FC<IconContainer> = ({ children, title }) => {
    return (
       <div className='flex flex-col group h-full w-full place-items-center'>
-         <motion.div whileHover={{ scale: 1.2 }} whileTap={{ rotate: 180 }}>
+         <motion.div whileHover={{ scale: 1.2, rotate: 360 }} whileTap={{ scale: 0 }}>
             {children}
          </motion.div>
-         <div className='relative invisible group-hover:visible group-active:visible mt-2 text-white py-0.5 rounded-md'>
+         <div className='relative group-hover:text-green-400 group-active:text-green-900 mt-2 text-white py-0.5 rounded-md'>
             {title}
          </div>
       </div>
