249b55f03fb7a5e171ad7d8571030ff07ce4cf88
mythosmystery <axiysfire@gmail.com>
2021-11-20 02:20:07 -0600

    minor ui changes

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -15,9 +15,10 @@ const Menu: FC<MenuProps> = ({ children, showMenu, handleClose }) => {
    return (
       <div
          className='top-0 left-0 mt-20 ml-3 fixed z-50 focus:outline-none'
-         onBlur={handleClose}
+         onBlur={() => setTimeout(() => handleClose(), 50)}
          tabIndex={-1}
-         ref={menuRef}>
+         ref={menuRef}
+      >
          <div className={showMenu ? '' : 'hidden'}>{children}</div>
       </div>
    );
@@ -30,7 +31,8 @@ const Button: FC<HTMLMotionProps<'div'>> = props => {
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 1.3, rotate: 90, translateY: 5 }}
             className='fixed sm:hidden m-2 p-4 top-0 left-0 transform text-gray-900 z-40 bg-green-400 rounded-full shadow-md active:shadow-sm'
-            {...props}>
+            {...props}
+         >
             <FaBars size='24' />
          </motion.div>
       </>
