0a7b605197b245fd87c516836210c2e531c6d825
mythosmystery <axiysfire@gmail.com>
2021-11-21 16:59:26 -0600

    updated gh pages links

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
    images: {
       loader: 'imgix',
-      path: isProd ? 'https://mythosmystery.github.io' : 'http:localhost:3000'
+      path: isProd ? 'https://mythosmystery.github.io' : 'http://localhost:3000'
    },
    basePath: '/tailwind-portfolio',
-   assetPath: '/tailwind-portfolio'
+   assetPrefix: '/tailwind-portfolio'
 };
