977fddd18afd4295aa93ae75aa3d36245a3cc634
mythosmystery <axiysfire@gmail.com>
2021-11-24 22:09:54 -0600

    updated

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,7 +23,8 @@ const Light: FC<CardProps> = ({ children, show }) => {
                initial={{ scaleY: 0 }}
                animate={{ scaleY: 1 }}
                exit={{ scaleY: 0 }}
-               className={`flex flex-col bg-gray-700 sm:w-3/5 mx-6 px-4 sm:px-9 pt-1 pb-4 my-4 sm:mx-auto rounded-md shadow-lg`}>
+               className={`flex flex-col bg-gray-700 sm:w-3/5 mx-6 px-4 sm:px-9 pt-1 pb-4 my-4 sm:mx-auto rounded-md shadow-lg`}
+            >
                {children}
             </motion.div>
          )}
@@ -38,7 +39,7 @@ interface FooterProps {
 const Footer: FC<FooterProps> = ({ children }) => {
    return (
       <div className='flex flex-row mt-2'>
-         {children.map((child, i) => {
+         {children.map((child: ReactNode, i) => {
             return (
                <motion.div
                   whileHover={{ scale: 1.05 }}
@@ -46,7 +47,8 @@ const Footer: FC<FooterProps> = ({ children }) => {
                   key={i}
                   className={
                      'border-t border-gray-700 w-2/4 text-center hover:text-yellow-600 text-lg py-3 text-indigo-600 hover:bg-gray-700 hover:cursor-pointer hover:shadow-md'
-                  }>
+                  }
+               >
                   {child}
                </motion.div>
             );
